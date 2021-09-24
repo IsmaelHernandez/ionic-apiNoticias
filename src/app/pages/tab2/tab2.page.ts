ba0b8b6b0360307fcebba5d5444e391986c3938b
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2021-09-24 13:47:40 -0500

    encabezados

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+
+  //tomar el elemento del segmento
+  @ViewChild(IonSegment) segment: IonSegment;
+
+  //creamos un arreglo llamadoi categorias
+  categorias = ['business','entertainment','general','health','science','sports','technology'];
+
 
   constructor() {}
+
+  ngOnInit(){
+    this.segment.value = this.categorias[0];
+  }
 
 }
